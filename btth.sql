1440ab38205fcb3a724fed3404707ef95881bb9d
TungCorn <tung01012005s@gmail.com>
2025-11-26 10:58:21 +0700

    Add dynamic cursors and stored procedures for order and revenue calculations

--- btth.sql
+++ btth.sql
@@ -30,7 +30,7 @@ select
     ) as ten
 from KhachHang
 
---c
+--d
 
 declare ttdonhang cursor dynamic scroll
 for
@@ -51,6 +51,87 @@ end
 close ttdonhang
 deallocate ttdonhang
 
+--e
+
+declare tongsotien cursor dynamic scroll
+for
+    select sum(TongTien)
+    from DonHang dh
+    join KhachHang kh on dh.IDKhachHang = kh.IDKhachHang
+    where kh.HoTen = N'Nguyễn Văn An'
+open tongsotien
+
+declare @tongsotien float
+fetch first from tongsotien into @tongsotien
+print 'Tong so tien la: ' + format(@tongsotien, 'N2')
+
+close tongsotien
+deallocate tongsotien
 
 
+--f
 
+create procedure doanh_thu_moi_ngay
+as
+begin
+    select NgayDatHang, sum(TongTien)
+    from DonHang dh
+    group by NgayDatHang
+end
+
+exec doanh_thu_moi_ngay
+
+drop procedure doanh_thu_moi_ngay
+--g
+
+create procedure doanh_thu_1_ngay
+    @ngay varchar(2)
+as
+begin
+    select NgayDatHang, sum(TongTien)
+    from DonHang dh
+    group by NgayDatHang
+    having day(NgayDatHang) = @ngay
+end
+
+exec doanh_thu_1_ngay '12'
+
+--h
+
+create procedure tong_tien_cua_1_khach_hang
+    @id int
+as
+begin
+    select IDKhachHang, sum(TongTien) as TongTien
+    from DonHang dh
+    group by IDKhachHang
+    having IDKhachHang = @id
+end
+
+exec tong_tien_cua_1_khach_hang 1
+
+
+select *
+from DonHang;
+
+--i
+
+create procedure tong_tien_don_hang
+    @ma_don_hang int,
+    @kq_tong_tien float output
+as
+begin
+    select @kq_tong_tien = TongTien
+    from DonHang
+    where IDDonHang = @ma_don_hang
+end
+
+declare @tong_tien_dh float
+
+exec tong_tien_don_hang
+    1,
+    @tong_tien_dh output ;
+
+print format(@tong_tien_dh, 'N2')
+
+    DROP PROCEDURE IF EXISTS tong_tien_don_hang;
